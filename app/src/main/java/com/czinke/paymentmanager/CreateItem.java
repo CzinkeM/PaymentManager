35561f5731dc85350e504c5be1d9a14f2e5af25e
Czinke Márton <czinkemarci@mailbox.unideb.hu>
2020-08-13 20:44:50 +0200

    apply some changes

--- app/src/main/java/com/czinke/paymentmanager/CreateItem.java
+++ app/src/main/java/com/czinke/paymentmanager/CreateItem.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.czinke.paymentmanager.models.Item;
+
 public class CreateItem extends AppCompatActivity {
+    public Item item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_item);
+
     }
 }
