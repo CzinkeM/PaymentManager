bc23647583985a07f1024293b61d8aefe0519190
Czinke Márton <czinkemarci@mailbox.unideb.hu>
2020-08-14 12:34:21 +0200

    Create AddItem layout

--- app/src/main/java/com/czinke/paymentmanager/CreateItem.java
+++ app/src/main/java/com/czinke/paymentmanager/CreateItem.java
@@ -3,16 +3,30 @@ package com.czinke.paymentmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.czinke.paymentmanager.models.Item;
 
+import java.util.Calendar;
+
 public class CreateItem extends AppCompatActivity {
     public Item item;
+    public TextView textName;
+    public TextView textDescription;
+    public CalendarView calendarDate;
+    public int intPrice;
+    public int priority;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_item);
+        InitializeViews();
+
+    }
+    private void InitializeViews()
+    {
 
     }
 }
